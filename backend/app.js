da072c68511deea9ffec63ede2542234389cf981
Marvin Hackfort <marvin.hackfort@gmail.com>
2023-04-27 20:00:48 +0200

    added backend stuff

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
 const express = require ('express');
 const app = express();
 const PORT = 3001;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require ('multer');
 
+app.use(cors());
+
+//connection to the database that marvin created
+app.use(async function (req, res, next) {
+    await mongoose.connect(
+      "mongodb+srv://marvimarv:voLFoE5hHcQ1XZBg@db-bucketlistbuddies.kyh0mlf.mongodb.net/test"
+    );
+    next();
+});
+
+
+//create a disk storage for all the uploads
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null,'uploads')
+    }
+    filename: function(req,file,cb) {
+        cb(null,`${file.originalname}-${Date.now()}`)
+    }
+})
+
+//all the different routes for the database
 
 app.get("/", (req,res) => {
     res.send("hallo welt")
