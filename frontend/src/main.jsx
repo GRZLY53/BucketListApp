e6175870dbaa2f70aac499fdb80ec5932d34a925
Marvin Hackfort <marvin.hackfort@gmail.com>
2023-04-27 21:01:07 +0200

    update routing and landingpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Landingpage from './components/Startingpage/landingpage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <Landingpage text="Hallo" />
   </React.StrictMode>,
 )
